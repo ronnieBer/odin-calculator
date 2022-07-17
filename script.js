036c13a4f68cf0b723a36c5a4bf9a01411e3d86d
Ronnie <ronniebercero@gmail.com>
2022-07-17 11:24:49 +0800

    Added an operate funtion for math operator

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ const subtract = (num1, num2) => num1 - num2;
 const multiply = (num1, num2) => num1 * num2;
 const divide = (num1, num2) => num1 / num2;
 // console.log(divide(24, 12));
+
+function operate(operator, firstOperand, secondOperand) {
+    if (operator === '+') return add(firstOperand, secondOperand);
+    if (operator === '-') return subtract(firstOperand, secondOperand);
+    if (operator === '*') return multiply(firstOperand, secondOperand);
+    if (operator === '/') return divide(firstOperand, secondOperand);
+};
+// console.log(operate('/', 24, 12));
